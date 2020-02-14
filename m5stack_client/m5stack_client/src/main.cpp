f40af33aee7f89dddf90e62e68a5b673d14f0337
Adam Fox <af16371@my.bristol.ac.uk>
2020-02-14 11:31:51 +0000

    Refactor main.cpp

--- m5stack_client/m5stack_client/src/main.cpp
+++ m5stack_client/m5stack_client/src/main.cpp
@@ -20,8 +20,8 @@ void setup() {
     USE_SERIAL.println();
     USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    for(int t = 0; t < 4; t++) {
+        USE_SERIAL.printf("[BOOT] %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
     }
@@ -29,46 +29,50 @@ void setup() {
     wifiMulti.addAP(WIFI_SSID, WIFI_PASS);
 }
 
-void loop() {
-    // wait for WiFi connection
-    if((wifiMulti.run() == WL_CONNECTED)) {
+String httpGET(String url) {
+    HTTPClient http;
+    String payload = "";
 
-        HTTPClient http;
+    http.begin(url);
+    int httpCode = http.GET();
 
-        USE_SERIAL.print("[HTTP] begin...\n");     
-        http.begin("http://192.168.0.38:5000/playback-state"); //HTTP
+    USE_SERIAL.printf("[HTTP] GET\n");
 
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-
-                //JSON PARSE
-                JSONVar obj = JSON.parse(payload); 
-                 
-                USE_SERIAL.println(payload);               
-                USE_SERIAL.print("item : ");               
-                USE_SERIAL.println((const char*) obj["item"]["name"]);
-
-                M5.Lcd.fillScreen(TFT_BLACK);
-                M5.Lcd.print((const char*) obj["item"]["name"]);
-                //M5.Lcd.drawPngUrl((const char*) obj["item"]["album"]["images"][0]["url"], 100, 100);
-                //M5.Lcd.drawPngUrl("https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png", 0, 0);
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
+    if(httpCode > 0) {
+      if(httpCode == HTTP_CODE_OK) {
+        payload = http.getString();
+        USE_SERIAL.printf("[HTTP] payload received\n");
+      }
+    } else {
+      USE_SERIAL.printf("[HTTP] GET failed w/ error: %s\n", http.errorToString(httpCode).c_str());
     }
 
-    delay(5000);
+    http.end();
+    return payload;
+}
+
+void displayText(String text, int x, int y, int size) {
+  M5.Lcd.setTextWrap(false, false);
+  M5.Lcd.setCursor(x, y);
+  M5.Lcd.setTextSize(size);
+  M5.Lcd.print(text);
+}
+
+void loop() {
+    if((wifiMulti.run() == WL_CONNECTED)) {
+        String payload = httpGET("http://192.168.0.38:5000/playback-state");
+        JSONVar obj = JSON.parse(payload); 
+         
+        //USE_SERIAL.println(payload);               
+        //USE_SERIAL.println((const char*) obj["item"]["name"]);
+
+        M5.Lcd.fillScreen(TFT_BLACK);
+        displayText((const char*) obj["item"]["name"], 10, 10, 1);
+        displayText((const char*) obj["item"]["album"]["name"], 10, 20, 1);
+        displayText((const char*) obj["item"]["artists"][0]["name"], 10, 30, 1);
+        //M5.Lcd.drawPngUrl((const char*) obj["item"]["album"]["images"][0]["url"], 100, 100);
+        //M5.Lcd.drawPngUrl("https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png", 0, 0);
+    }
+
+    delay(2000);
 }
