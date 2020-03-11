b36b6e3d43f24b76bb0b72648d8eb35bca7e062f
Charlie Harding <charlie_harding@icloud.com>
2020-03-11 15:48:52 +0000

    Fix Active component

--- react_client/src/Active.jsx
+++ react_client/src/Active.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Active = ({ activeImageUrl }) => (
   <div className="active" style={{ ...!activeImageUrl && { opacity: 0 } }}>
-    <img src={activeImageUrl} height="240" width="240" alt="" />
+    <img src={activeImageUrl || null} height="240" width="240" alt="" />
   </div>
 );
 export default Active;
