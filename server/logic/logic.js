cee7fe2b95623cfbf37bc4e4f6e6b1e56c076fcc
Adam Fox <af16371@my.bristol.ac.uk>
2020-04-17 15:26:17 +0100

    [backend] Move stack-sending functionality to backend

--- server/logic/logic.js
+++ server/logic/logic.js
@@ -2,6 +2,7 @@ const express = require('express');
 var cors = require("cors");
 const http = require('http');
 const webSocketServer = require('websocket').server;
+const axios = require('axios');
 
 // Express
 const app = express();
@@ -47,3 +48,82 @@ app.post('/frontend/send', function(req, res) {
 });
 
 app.listen(port, () => { console.log(`logic REST interface: ${port}`); });
+
+// SPOTIFY ------------------------------ 
+
+const host = 'localhost';
+const spotifyPort = 8004;
+const utilsPort = 8005;
+const clientControllerPort = 8002;
+
+app.get('/spotify/send/top-tracks', function(req, res) {
+  updateClientList();
+
+  axios.get(`http://${host}:${spotifyPort}/spotify/top-tracks`)
+    .then(function (response) {
+      sendTracksToStacks(response.data);
+      res.status(200).send();
+    })
+});
+
+// STACKS ------------------------------ 
+let clientList = [];
+const clientState = new Map();
+
+function sendTracksToStacks(list) {
+  clientList.forEach((element) => {
+    const mac = element[0];
+
+    // TODO: implement currently playing stack
+
+    const index = Math.floor(Math.random() * (list.length - 1));
+    const spotifyPayload = list[index];
+    list.splice(index, 1);
+  
+    console.log(`${element[0]}: ${spotifyPayload.name}`);
+
+    // Send text
+    clearStackText(mac);
+    sendTextToStack(mac, 10, 250, spotifyPayload.name);
+    sendTextToStack(mac, 10, 265, spotifyPayload.album.name);
+    sendTextToStack(mac, 10, 280, spotifyPayload.artists[0].name);
+
+    // Convert and send image
+    axios.post(`http://${host}:${utilsPort}/convert/jpeg-to-png?jpegUrl=${spotifyPayload.album.images[0].url}`)
+      .then(function (response) {
+        sendImageToStack(mac, 0, 0, response.data);
+      })
+
+    clientState.set(mac, spotifyPayload);
+    sendUpdatedStateToFrontend(mac, spotifyPayload);
+  });
+}
+
+function sendUpdatedStateToFrontend(mac, spotifyPayload) {
+  const json = { mac, spotifyPayload };
+
+  if (frontend != null ) {
+    frontend.sendUTF(JSON.stringify(json));
+  }
+}
+
+function updateClientList() {
+  axios.get(`http://${host}:${clientControllerPort}/client/list`)
+    .then(function (response) {
+      clientList = response.data;
+    })
+}
+
+function clearStackText(id) {
+  axios.post(`http://${host}:${clientControllerPort}/send/clear?mac=${id}`);
+}
+
+function sendTextToStack(id, x, y, text) {
+  const bodyString = `mac=${id}&text=${text}&x=${x}&y=${y}`;
+  axios.post(`http://${host}:${clientControllerPort}/send/text?${bodyString}`);
+}
+
+function sendImageToStack(id, x, y, pngUrl) {
+  const bodyString = `mac=${id}&pngUrl=${pngUrl}&x=${x}&y=${y}`;
+  axios.post(`http://${host}:${clientControllerPort}/send/image?${bodyString}`);
+}
